058841242bba6e1faf147b9523f5140c571c7b5c
chris-hellon <38248279+chris-hellon@users.noreply.github.com>
2021-07-02 12:58:58 +0700

    Add files via upload

---
+++ samples/rest-api-to-excel/Models/ExchangeInfoModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Binance.Models
+{
+    public class ExchangeInfoModel
+    {
+        [JsonProperty(PropertyName = "timeZone")]
+        public string TimeZone { get; set; }
+
+        [JsonProperty(PropertyName = "serverTime")]
+        public long ServerTime { get; set; }
+
+        [JsonProperty(PropertyName = "futuresType")]
+        public string FuturesType { get; set; }
+
+        [JsonProperty(PropertyName = "symbols")]
+        public List<SymbolModel> Symbols { get; set; }
+
+        public ExchangeInfoModel()
+        {
+
+        }
+    }
+}
